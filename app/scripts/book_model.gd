0572346cf54ba7b71ae3332ba855392de79fe47d
Nordup <nordup.ondr@gmail.com>
2024-08-17 23:21:01 +0400

    clean up, retina display

--- app/scripts/book_model.gd
+++ app/scripts/book_model.gd
@@ -1,7 +1,8 @@
 @tool
 extends Node3D
 
-@export var multimeshInstance: MultiMeshInstance3D
+@export var root: Node3D
+@export var meshInstance: MeshInstance3D
 @export var pages: int
 
 @export_category("Debug")
@@ -10,23 +11,41 @@ extends Node3D
 		if not Engine.is_editor_hint(): return
 		instance_pages()
 
+@export var clear: bool:
+	set(value):
+		if not Engine.is_editor_hint(): return
+		clear_pages()
+
 var page_size = Vector3(1, 0.0005, 1)
-var multimesh: MultiMesh
 
 
 func _ready() -> void:
-	multimesh = multimeshInstance.multimesh
 	instance_pages()
 
 
 func instance_pages() -> void:
-	multimesh.instance_count = pages
-	(multimesh.mesh as BoxMesh).size = page_size
+	(meshInstance.mesh as BoxMesh).size = page_size
 	
+	# Clear prev
+	clear_pages()
+	
+	# Create new
 	for i in range(pages):
-		var pos = Transform3D()
+		var page = MeshInstance3D.new()
+		page.mesh = meshInstance.mesh
+		page.material_override = meshInstance.material_override
+		
+		var page_transform = Transform3D()
 		var y = i * page_size.y
-		pos = pos.translated(Vector3(0, y, 0))
-		multimesh.set_instance_transform(i, pos)
+		page_transform = page_transform.translated(Vector3(0, y, 0))
+		
+		page.transform = page_transform
+		
+		root.add_child(page)
 	
 	print("Instanced " + str(pages) + " pages")
+
+
+func clear_pages() -> void:
+	for page in root.get_children():
+		page.queue_free()
